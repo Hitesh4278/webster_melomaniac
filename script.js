74b303ce6419e90387881cfbd9cc1d322fd65723
Hitesh <hiteshgahanolia@gmail.com>
2022-11-08 23:05:24 +0530

    Played Music

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ console.log("Musixon_Avishkar");
 
 let songIndex = 0;
 let audioelement = new Audio('1.mp3');
-// audioelement.play();
+
 let masterplay = document.getElementById('masterPlay');
-let musicBar = document.getElementsById('musicBar');
+// let musicBar = document.getElementsById('musicBar');
 let songs = [
-    {songname: "Phenomenon", filePath: "songs/1.mp3", coverPath: "cover/1.jpg" },
-    {songname: "Phenomenon", filePath: "songs/2.mp3", coverPath: "cover/1.jpg"},
-    {songname: "Phenomenon", filePath: "songs/3.mp3", coverPath: "cover/1.jpg"},
+    {songname: "Phenomenon", filePath: "song/1.mp3", coverPath: "cover/1.jpg" },
+    {songname: "Phenomenon", filePath: "2.mp3", coverPath: "cover/1.jpg"},
+    {songname: "Phenomenon", filePath: "3.mp3", coverPath: "cover/1.jpg"},
 
 ]
 
 masterplay.addEventListener('click', ()=>{
     if(audioelement.paused || audioelement.currentTime<=0){
         audioelement.play();
-        masterplay.classList.remove('fa-solid fa-play');
-        masterplay.classList.add('fa-solid fa-pause');
+        masterplay.classList.remove('fa-play-circle');
+        masterplay.classList.add('fa-pause-circle');
+    }
+    else{
+        audioelement.pause();
+        masterplay.classList.remove('fa-pause-circle');
+        masterplay.classList.add('fa-play-circle');
     }
 } )
