f3bb02ab3e1b2d36bf4f4f433d2cb2b90bbd6b25
Hitesh <hiteshgahanolia@gmail.com>
2022-11-08 23:52:01 +0530

    Changing the properties of music bar and gif

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioelement = new Audio('1.mp3');
 
 let masterplay = document.getElementById('masterPlay');
+let gif  = document.getElementById('gif');
 // let musicBar = document.getElementsById('musicBar');
 let songs = [
     {songname: "Phenomenon", filePath: "song/1.mp3", coverPath: "cover/1.jpg" },
@@ -17,10 +18,16 @@ masterplay.addEventListener('click', ()=>{
         audioelement.play();
         masterplay.classList.remove('fa-play-circle');
         masterplay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
     }
     else{
         audioelement.pause();
         masterplay.classList.remove('fa-pause-circle');
         masterplay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
     }
 } )
+
+
+
+
